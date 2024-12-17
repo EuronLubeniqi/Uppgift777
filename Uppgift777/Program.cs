f6f5b4401d0bc34cb3e104407328a639c0fe4633
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-12-17 13:42:37 +0100

    Add project files.

---
+++ Uppgift777/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int nummer;
+
+        Console.WriteLine("Mata in ett positivt tal. Skriv ett negativt tal för att avsluta.");
+
+        while (true)
+        {
+            Console.Write("Ange ett tal: ");
+            nummer = int.Parse(Console.ReadLine());
+
+            if (nummer < 0)
+            {
+                Console.WriteLine("Du skrev ett negativt tal. Programmet avslutas.");
+                break;
+            }
+        }
+    }
+}
